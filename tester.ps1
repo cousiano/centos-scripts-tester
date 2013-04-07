f3a4a5e591e59585c5e5a3a8148506ed8ae1fd78
cousiano <marc.rabahi@gmail.com>
2013-04-07 16:21:36 +0200

    copy, configure and launch vmware.

--- tester.ps1
+++ tester.ps1
@@ -16,8 +16,10 @@
 ################################
 $breakLine="`n"
 
+$vmware_exe="C:/Program Files (x86)/VMware/VMware Player/vmplayer.exe"
 $vmware_home="$HOME/Documents/Virtual Machines"
 $vmware_template="centos"
+$vmware_list="srv-build-linux","srv-cit"
 
 ################################
 # PREREQUISTES
@@ -39,6 +41,31 @@ If ($intAnswer -ne 6)
 ################################
 # SCRIPT
 ################################
+
 echo "remove other Virtual Machines"
 dir "$vmware_home" | where { ($_.name -ne "$vmware_template") -and ($_.extension -ne ".iso")}| Remove-Item -Recurse -force
 
+echo "now duplicate vmware"
+foreach ($item in $vmware_list)
+{
+  echo "working with $item"
+  
+  # duplicate item:
+  Copy-Item "$vmware_home/$vmware_template" "$vmware_home/$item" -recurse
+  
+  # rename files:
+  Get-ChildItem "$vmware_home/$item" | Rename-Item -NewName { $_.Name -replace "^$vmware_template+","$item"}
+    
+  # replace $vmware_template by $item in configuration files:
+  Get-Content "$vmware_home/$item/$item.vmx" | %{$_ -replace "$vmware_template+","$item"} | Set-Content "$vmware_home/$item/$item.vmx.output"  
+  Move-Item "$vmware_home/$item/$item.vmx.output" "$vmware_home/$item/$item.vmx" -force
+  
+  Get-Content "$vmware_home/$item/$item.vmdk" | %{$_ -replace "$vmware_template+","$item"} | Set-Content "$vmware_home/$item/$item.vmdk.output"  
+  Move-Item "$vmware_home/$item/$item.vmdk.output" "$vmware_home/$item/$item.vmdk" -force
+  
+  Get-Content "$vmware_home/$item/$item.vmxf" | %{$_ -replace "$vmware_template+","$item"} | Set-Content "$vmware_home/$item/$item.vmxf.output"  
+  Move-Item "$vmware_home/$item/$item.vmxf.output" "$vmware_home/$item/$item.vmxf" -force
+  
+  # now launch vmware:
+  & $vmware_exe "$vmware_home/$item/$item.vmx"
+}
